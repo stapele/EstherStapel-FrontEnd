9477ffc81f431b12c56c06ac4fc3695aef02a93a
stapele <esther.stapel@hva.nl>
2020-01-22 02:16:32 +0100

    Add files via upload

---
+++ js/kaas.js
@@ -0,0 +1,7 @@
+var zoekveld = document.querySelector('header form');
+var zoekbutton = document.querySelector('header button');
+
+
+zoekbutton.addEventListener("click", function () {
+    zoekveld.classList.toggle('show-search');
+});
